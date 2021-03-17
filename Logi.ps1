636de17a676ca34447a204ccee48bc4f6904082e
classicaws <80813269+classicaws@users.noreply.github.com>
2021-03-17 14:31:50 +0530

    Add files via upload

---
+++ Logi.ps1
@@ -0,0 +1,3 @@
+﻿Copy-S3Object -BucketName classic-aws-master -Key lgx120102.lic -LocalFile C:\lgx120102.lic -AccessKey AKIAZWJOQL2D7GP6FS5W -SecretKey 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 -Region us-east-1
+Rename-Item C:\Ephesoft\EphesoftReports\lgx120102.lic lgx120102.bkp
+Copy-Item C:\lgx120102.lic C:\Ephesoft\EphesoftReports\
